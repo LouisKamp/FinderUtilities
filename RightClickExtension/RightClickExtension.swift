ed1af159c34180aa71b496e8cdd61b758ce8905a
Louis Kamp Eskildsen <31962148+LouisKamp@users.noreply.github.com>
2023-06-10 19:29:18 +0200

    Open Iterm2 and VSC

--- RightClickExtension/RightClickExtension.swift
+++ RightClickExtension/RightClickExtension.swift
@@ -24,9 +24,10 @@ class FinderSync: FIFinderSync {
 
         // Produce a menu for the extension (to be shown when right clicking a folder in Finder)
         let menu = NSMenu(title: "")
-        menu.addItem(withTitle: "Open Terminal", action: #selector(openTerminalClicked(_:)), keyEquivalent: "")
-        menu.addItem(withTitle: "Create empty.txt", action: #selector(createEmptyFileClicked(_:)), keyEquivalent: "")
+        menu.addItem(withTitle: "Open Terminal here", action: #selector(openTerminalClicked(_:)), keyEquivalent: "")
+        menu.addItem(withTitle: "Create new textfile", action: #selector(createEmptyFileClicked(_:)), keyEquivalent: "")
         menu.addItem(withTitle: "Copy selected paths", action: #selector(copyPathToClipboard), keyEquivalent: "")
+        menu.addItem(withTitle: "Open in Visual Studio Code", action: #selector(openVSCClicked), keyEquivalent: "")
 
         return menu
     }
@@ -35,7 +36,6 @@ class FinderSync: FIFinderSync {
     @IBAction func copyPathToClipboard(_ sender: AnyObject?) {
         
         guard let target = FIFinderSyncController.default().selectedItemURLs() else {
-            
             
             NSLog("Failed to obtain targeted URLs: %@")
 
@@ -68,7 +68,7 @@ class FinderSync: FIFinderSync {
         
         let task = Process()
         task.executableURL = URL(fileURLWithPath: "/usr/bin/open")
-        task.arguments = ["-a", "terminal", "\(target)"]
+        task.arguments = ["-a", "iTerm", "\(target)"]
         
         do {
             
@@ -77,6 +77,52 @@ class FinderSync: FIFinderSync {
         } catch let error as NSError {
             
             NSLog("Failed to open Terminal.app: %@", error.description as NSString)
+        }
+    }
+    
+    @IBAction func openVSCClicked(_ sender: AnyObject?) {
+        
+        guard let target_folder = FIFinderSyncController.default().targetedURL() else {
+            
+            NSLog("Failed to obtain targeted URL: %@")
+            
+            return
+        }
+        
+        
+        guard let target_files = FIFinderSyncController.default().selectedItemURLs() else {
+            
+            NSLog("Failed to obtain targeted URLs: %@")
+
+            return
+        }
+        
+        
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/bin/open")
+        task.arguments = ["-a", "Visual Studio Code"]
+        
+        var open_target_folder = true
+        
+        target_files.forEach { file in
+            if (file.hasDirectoryPath) {
+                open_target_folder = false
+            }
+            task.arguments?.append(file.path)
+        }
+        
+        if (open_target_folder) {
+            task.arguments?.append("\(target_folder.path)")
+        }
+        
+        
+        do {
+            
+            try task.run()
+
+        } catch let error as NSError {
+            
+            NSLog("Failed to open Visual Studio Code.app: %@", error.description as NSString)
         }
     }
 
